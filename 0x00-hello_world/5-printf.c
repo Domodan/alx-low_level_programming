a1898d44e708a4505ea8274cea6b8616d5fecce8
Domodan <ongomdaniel9@gmail.com>
2023-03-16 11:42:23 -0700

    display with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Display a message using puts function
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+        printf("\"with proper grammar, but the outcome is a piece of art,");
+        return (0);
+}
