567cc831e0df01c871805b71b078821f92d6a91e
Domodan <ongomdaniel9@gmail.com>
2023-03-29 13:13:05 -0700

    reverse array contents

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+/**
+ * reverse_array - The function reverses the content of an array of integers
+ *
+ * @a: the array
+ * @n: size of array
+ *
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = a[j];
+			a[j] = a[(j - 1)];
+			a[(j - 1)] = temp;
+		}
+	}
+}
