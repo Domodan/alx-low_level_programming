f916c330cc3d82c88523cb6bf446629950e60136
Domodan <ongomdaniel9@gmail.com>
2023-04-17 00:49:41 -0700

    return number raised to power of another number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _pow_recursion - The function returns the value of x raised to the power of y
+ *
+ * @x: base number
+ * @y: exponent number
+ *
+ * Return: x raised to the power of y, -1 otherwise
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
