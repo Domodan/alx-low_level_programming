f61f4761f5f1aa26327e4ce2b1295986d1de8ba0
Domodan <ongomdaniel9@gmail.com>
2023-04-17 03:11:26 -0700

    defined a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data structure of a dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: dog with name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
