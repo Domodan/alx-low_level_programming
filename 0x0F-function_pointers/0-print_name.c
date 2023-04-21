838171fff65d798ff838f1fa33f0e0bc53cdbef6
Domodan <ongomdaniel9@gmail.com>
2023-04-21 12:51:29 -0700

    print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+/**
+ * print_name - the function prints a name.
+ *
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
