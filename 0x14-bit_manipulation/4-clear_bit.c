17ad65143816b87c9e95d5e088c87570aefa03a1
Domodan <ongomdaniel9@gmail.com>
2023-05-04 13:35:08 -0700

    modified

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,8 +6,7 @@
  * @n: A pointer to the bit.
  * @index: The index to set the value at - indices start at 0.
  *
- * Return: If an error occurs - -1.
- * 		Otherwise - 1.
+ * Return: If an error occurs - -1. Otherwise - 1.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
