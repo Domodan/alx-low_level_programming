7217ee40523bd459e5ba412e98c61f872405fffa
Domodan <ongomdaniel9@gmail.com>
2023-06-19 10:07:04 -0700

    a script that creates a dynamic library called liball.so from all the .c files that are in the current directory

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-gcc -fPIC -c *.c
-gcc -shared -Wl,-soname,liball.so -o liball.so *.o
+
+# Compile all .c files in the current directory into object files
+gcc -c *.c
+
+# Create the dynamic library liball.so from the object files
+gcc -shared -o liball.so *.o
+
+# Clean up by removing the object files
+rm *.o
